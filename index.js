fe7fc40e453ebbbc3511a292f788c164b2ed9c5b
Hamza-Laghari <120000289+Hamza-Laghari@users.noreply.github.com>
2023-01-06 07:13:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+console.log(countEl)
+
+let count = 0
+
+function increment() { 
+   count++;
+   countEl.innerText = count;
+}
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+function reset(){
+    saveEl.textContent = "Previous enteries Deleted"
+}
